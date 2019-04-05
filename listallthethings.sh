906c6a205b7a9d4bd207228139dba55cd6945cb8
Jake <gr8t1999@gmail.com>
2019-04-05 13:10:16 -0400

    editted bug

--- listallthethings.sh
+++ listallthethings.sh
@@ -1,46 +1,48 @@
+isFound=1
 if [ $# -gt 0 ]
 then
     for args in $@
     do
 	
-    echo "proceeding $args"
-
+     
     count=$(ls -l | grep -c ^d)
-    echo "count: $count"
+   
     if [ "$count" -ne 0 ]
        then
        dirs=(*/)
-    
+    fi
     for d in "${dirs[@]}"
     do
 	cd "$d"
 	
 	val=$(find -name $args)
-	echo $val
+	
 	if [ -z "$val" ]
 	then
-	    echo not found
+	    isFound=0
+	    
 	    cd ..
 	    
 	else
-	    ls -i $val
-	    
-	    echo found
+	    ls -li $val
+	    isFound=1
+	   
 	    cd ..
 	    break 1
 	    
 	fi
 	
     done
-    else
-	ls -l | grep 
-    fi    
+    if [ $isFound -eq 0 ]
+       then
+	
+	ls -li $args 
+    fi
+    
         
         
 done
 else
-    echo HI
-
-
+    echo No arguments given
 
 fi
